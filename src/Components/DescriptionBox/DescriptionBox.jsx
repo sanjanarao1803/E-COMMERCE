4f8b45ab3130fe4e48d2109724677712496980bc
Sanjana Rao <sanjanarao1803@gmail.com>
2024-05-09 15:11:44 +0530

    gh-pages

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css';
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className='descriptionbox-nav-box fade'>Reviews (122)</div>
+        </div>
+        <div className='descriptionbox-description'>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum, sequi suscipit voluptate similique totam labore maiores obcaecati molestias alias, quaerat voluptas! Maiores, illum. Obcaecati velit reiciendis voluptate dolorum veritatis iure!Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus labore magni cupiditate laborum quo expedita odio quasi nam, dignissimos magnam soluta, autem sapiente similique nihil nemo itaque ullam at voluptatibus?
+            </p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores voluptatibus magni deleniti fuga et sit culpa eligendi velit. Soluta non veniam, est voluptatem perferendis necessitatibus atque recusandae distinctio? Consectetur, minima.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
